62d88092ca228a28b4f688bfa6d06e94f0f9b5d4
Tim Harrison <TimHarrison1260@gmail.com>
2012-10-20 15:27:35 +0100

    ASPWD2 - Modules 6 - Answers to Lab Exercies

---
+++ ViewsLab/ViewsLab/Controllers/UserController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using ViewsLab.Domain;
+using ViewsLab.Models;
+
+using System;
+
+namespace ViewsLab.Controllers
+{
+    public class UserController : Controller
+    {
+        IUserRepository repository;
+
+        public UserController(IUserRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        //
+        // GET: /User/
+
+        public ActionResult Index(string pageSize, string page, string sort)
+        {
+            // TODO: complete this method
+            //  Pass the parameter to the view in the ViewBag dictionary
+            ViewBag.pageSize = pageSize;
+            ViewBag.page = page;
+            ViewBag.sort = sort;
+
+            return View();
+        }
+
+        public ActionResult UserGrid(string pageSize, string page, string sort)
+        {
+            // TODO: complete this method
+            //  Assign Default values to parameters if null.
+            int pSize = (pageSize == null ? 5 : Convert.ToInt32(pageSize));
+
+            //  The 'page' value passed in is obtained from the page bar at
+            //  the bottom of the WebGrid, this is the absolute page number.
+            //  However, the repository uses it as a Zero based index number
+            //  as does the WebGrid, therefore, we must adjust this absolute
+            //  page passed in to match the index.
+            int p = (page == null ? 0 : (Convert.ToInt32(page) - 1));     
+            
+            //  get the page of users and the total number of users 
+            //  from the repository
+            var users = repository.Get(pSize, p, sort);
+            int usersCount = repository.Count();
+            
+            //  Construct the ViewModel for the UserGrid
+            var usersView = new UserGridModel()
+            {
+                Users = users,
+                PageSize = pSize,
+                CurrentPage = p,    
+                RowCount = usersCount
+            };
+            //  Return the view model
+            return PartialView(usersView);
+        }
+
+        public ActionResult UserDetail(string username)
+        {
+            ViewBag.Username = username;
+            return View();
+        }
+  
+    }
+}
---
+++ ViewsLab/ViewsLab/Helpers/ImageLinkHelper.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace ViewsLab.Helpers
+{
+    public static class ImageLinkHelper
+    {
+        public static MvcHtmlString ImageLink(this HtmlHelper helper, string actionName, 
+            string imageUrl, string alternateText, object routeValues, 
+            object linkHtmlAttributes, object imageHtmlAttributes)
+        {
+            //  Create an instance of the url helper class
+            var urlHelper = new UrlHelper(helper.ViewContext.RequestContext);
+            
+            //  Set up variables for the parameters to be passed to the various methods
+            //  of the helper to construct the html.
+            //  Use the Action method to generate the correct Url
+            var url = urlHelper.Action(actionName, routeValues);
+            //  Add any attributes to the RouteValueDictionary
+            var linkAttributes = new RouteValueDictionary(linkHtmlAttributes);
+            //  Use the Content method to generate the url fot eh image (not an action url)
+            var imgUrl = urlHelper.Content(imageUrl);
+            //  Use the Encode method to ensure any text is property encoded, to stop scripting attacks
+            var imgAltText = urlHelper.Encode(alternateText);
+            var imgAttributes = new RouteValueDictionary(imageHtmlAttributes);
+
+            //  Create the Anchor tag to hold the href.
+            var linkTagbuilder = new TagBuilder("a");
+            //  Add the href attribute
+            linkTagbuilder.MergeAttribute("href", url);
+            //  Add any attributes passed into method.
+            linkTagbuilder.MergeAttributes(linkAttributes);
+
+            //  Create the img tag to contain the image
+            var imageTagBuilder = new TagBuilder("img");
+            //  Add the alt and src attributes
+            imageTagBuilder.MergeAttribute("alt", imgAltText);
+            imageTagBuilder.MergeAttribute("src", imgUrl);
+            //  Add any additional attributes passed into the method.
+            imageTagBuilder.MergeAttributes(imgAttributes);
+            
+            //  Add the img tag inside the Anchor tag.
+            linkTagbuilder.InnerHtml = imageTagBuilder.ToString(TagRenderMode.SelfClosing);
+
+            //  Wrap the html in an MvcHtmlString and return.
+            //  If we use the TagRenderMode.SelfClosing for this operation, it wipes out
+            //  the innerHtml.
+            return new MvcHtmlString(linkTagbuilder.ToString());
+        }
+
+        //  overload the helper method to offer less parameters
+        public static MvcHtmlString ImageLink(this HtmlHelper helper, string actionName,
+            string imageUrl, string alternateText, object routeValues)
+        {
+            return ImageLink(helper, actionName, imageUrl, alternateText, routeValues, null, null);
+        }
+
+        //  overload the helper method to offer less parameters
+        public static MvcHtmlString ImageLink(this HtmlHelper helper, string actionName,
+            string imageUrl, string alternateText)
+        {
+            return ImageLink(helper, actionName, imageUrl, alternateText, null, null, null);
+        }
+
+
+
+
+
+
+    }
+}
